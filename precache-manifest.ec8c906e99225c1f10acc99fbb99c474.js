7c3ac4c6b75d1a19bf0a606d0abd7d8e28e2e12f
Scott Smyth <scott@macintosh.local>
2019-09-10 13:40:50 -0400

    Updates

---
+++ precache-manifest.ec8c906e99225c1f10acc99fbb99c474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44585ca0fa80572d732be8c56594cdfa",
+    "url": "/Todo-list/index.html"
+  },
+  {
+    "revision": "90268ba36af60030a197",
+    "url": "/Todo-list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7f61d5b53d266efecaca",
+    "url": "/Todo-list/static/css/main.60d5cc2c.chunk.css"
+  },
+  {
+    "revision": "90268ba36af60030a197",
+    "url": "/Todo-list/static/js/2.53d176ea.chunk.js"
+  },
+  {
+    "revision": "7f61d5b53d266efecaca",
+    "url": "/Todo-list/static/js/main.6b26b644.chunk.js"
+  },
+  {
+    "revision": "cbb164a63665cbb7e88c",
+    "url": "/Todo-list/static/js/runtime~main.99151126.js"
+  }
+]);
